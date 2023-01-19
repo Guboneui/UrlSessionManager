ccdfceae472ec3681f3a72e41bbabd719b4b6355
보니 <73548875+Guboneui@users.noreply.github.com>
2023-01-19 22:03:37 +0900

    HTTPMethod enum 케이스

---
+++ UrlSessionManager/UrlSessionManager.swift
@@ -0,0 +1,15 @@
+//
+//  UrlSessionManager.swift
+//  UrlSessionManager
+//
+//  Created by 구본의 on 2023/01/19.
+//
+
+import Foundation
+
+enum HttpMethod: String {
+  case GET = "GET"
+  case POST = "POST"
+  case PUT = "PUT"
+  case DELETE = "DELETE"
+}
