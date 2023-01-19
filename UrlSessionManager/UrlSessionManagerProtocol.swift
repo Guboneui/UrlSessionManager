d4277e883ca2b87e688e042d00a2018805d0fe55
보니 <73548875+Guboneui@users.noreply.github.com>
2023-01-20 00:42:39 +0900

    requestplane, queryString, requestParam 추가

--- UrlSessionManager/UrlSessionManagerProtocol.swift
+++ UrlSessionManager/UrlSessionManagerProtocol.swift
@@ -8,14 +8,28 @@
 import Foundation
 
 protocol UrlSessionManagerProtocol {
-  func makeRequest(url: String, method: HttpMethod) async -> URLRequest
-  
+  func makePlaneRequest(url: String, method: HttpMethod) async -> URLRequest
+  func makeQueryRequest(url: String, method: HttpMethod, query: [String:Any]) async -> URLRequest
 }
 
 extension UrlSessionManagerProtocol {
-  func makeRequest(url: String, method: HttpMethod) async -> URLRequest{
+  func makePlaneRequest(url: String, method: HttpMethod) async -> URLRequest{
     var request = URLRequest(url: URL(string: url)!)
     request.httpMethod = method.rawValue
+    return request
+  }
+  
+  func makeQueryRequest(url: String, method: HttpMethod, query: [String:Any]) async -> URLRequest {
+    var queryString = query.map{"?\($0)=\($1)"}.joined()
+    var request = URLRequest(url: URL(string: url+queryString)!)
+    request.httpMethod = method.rawValue
+    return request
+  }
+  
+  func makeParameterRequest(url: String, method: HttpMethod, parameter: [String:Any]) async -> URLRequest {
+    var request = URLRequest(url: URL(string: url)!)
+    request.httpMethod = method.rawValue
+    request.httpBody = try! JSONSerialization.data(withJSONObject: parameter, options: [])
     return request
   }
 }
